52ea98ec9ddca312a2cf231832a0161e2dd9be94
WonHyun <coy4728@naver.com>
2019-09-04 12:57:27 +0900

    basic implements todolist, todo

--- component/todo.js
+++ component/todo.js
@@ -1,28 +1,103 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  TextInput,
-} from 'react-native';
+import {StyleSheet, View, TouchableOpacity, TextInput} from 'react-native';
+import {CheckBox} from 'react-native-elements';
+import {Icon} from 'react-native-elements';
+import Accordion from 'react-native-collapsible';
 
 export default class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: this.props.todo,
+      isEditMode: false,
+    };
+  }
+
+  _toggleCompletedCheck = () => {
+    this.setState(prevState => ({
+      todo: {
+        ...prevState.todo,
+        isCompleted: !prevState.todo.isCompleted,
+      },
+    }));
+  };
+
+  _toggleEditMode = () => {
+    this.setState(prevState => {
+      return {
+        isEditMode: !prevState.isEditMode,
+      };
+    });
+  };
+
+  _changeTitleText = text => {
+    this.setState(prevState => ({
+      todo: {
+        ...prevState.todo,
+        title: text,
+      },
+    }));
+  };
+
+  _titleTextFocus = () => {
+    setTimeout(() => this.titleText.focus(), 0);
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.titleBar}>
-          <Text style={styles.titleText}>text</Text>
+        <CheckBox
+          style={styles.checkbox}
+          checked={this.state.todo.isCompleted}
+          onPress={this._toggleCompletedCheck}
+          checkedColor={'#9bc53b'}
+        />
+        <TouchableOpacity
+          style={styles.titleBar}
+          onLongPress={() => {
+            this._toggleEditMode();
+            this._titleTextFocus();
+          }}>
+          <TextInput
+            style={
+              this.state.todo.isCompleted
+                ? styles.CompletedTitleText
+                : styles.UncompletedTitleText
+            }
+            editable={this.state.isEditMode}
+            onChangeText={this._changeTitleText}
+            value={this.state.todo.title}
+            returnKeyType={'done'}
+            onEndEditing={this._toggleEditMode}
+            ref={ref => (this.titleText = ref)}
+          />
         </TouchableOpacity>
 
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button}>
-            <Text>📝</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <Text>❌</Text>
-          </TouchableOpacity>
-        </View>
+        {this.state.isEditMode ? (
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={this._toggleEditMode}>
+              <Icon name="check" color="#bbbbbb" />
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                this._toggleEditMode();
+                this._titleTextFocus();
+              }}>
+              <Icon name="create" color="#bbbbbb" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.props.deleteTodo(this.state.todo.createdAt)}>
+              <Icon name="delete" color="#bbbbbb" />
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     );
   }
@@ -33,22 +108,31 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 20,
     backgroundColor: '#472d30',
     borderRadius: 5,
     marginBottom: 10,
+    elevation: 5,
   },
   titleBar: {
-    flex: 1,
+    flex: 3,
   },
-  titleText: {
+  UncompletedTitleText: {
     fontSize: 20,
     color: '#ffffff',
   },
+  CompletedTitleText: {
+    fontSize: 20,
+    textDecorationLine: 'line-through',
+    color: '#bbbbbb',
+  },
   buttonContainer: {
+    flex: 1,
     flexDirection: 'row',
   },
   button: {
     marginLeft: 10,
+  },
+  checkbox: {
+    flex: 1,
   },
 });
--- layout/todolist.js
+++ layout/todolist.js
@@ -9,10 +9,54 @@ import {
   Dimensions,
 } from 'react-native';
 import Todo from '../component/todo';
+import {Icon} from 'react-native-elements';
 
-const {height, width} = Dimensions.get('window');
+const {width} = Dimensions.get('window');
 
 export default class TodoList extends React.Component {
+  state = {
+    todos: [],
+    newTodoTitle: '',
+  };
+
+  todoCallback = todoFromChild => {
+    this.setState(prevState => ({
+      todos: [todoFromChild, ...prevState.todos],
+    }));
+  };
+
+  _addTodo = () => {
+    const newTodo = {
+      title:
+        this.state.newTodoTitle !== ''
+          ? this.state.newTodoTitle
+          : 'New Todo Title',
+      description: '',
+      dueDate: '',
+      createdAt: Date.now(),
+      priority: 3,
+      isCompleted: false,
+    };
+    this.setState(prevState => ({
+      todos: [newTodo, ...prevState.todos],
+      newTodoTitle: '',
+    }));
+  };
+
+  _deleteTodo = createdAt => {
+    this.setState(prevState => {
+      const index = prevState.todos.findIndex(e => e.createdAt === createdAt);
+      prevState.todos.splice(index, 1);
+      return {
+        todos: [...prevState.todos],
+      };
+    });
+  };
+
+  _changeTitleText = text => {
+    this.setState({newTodoTitle: text});
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -21,13 +65,29 @@ export default class TodoList extends React.Component {
             style={styles.inputText}
             placeholder={'New Todo title'}
             autoCorrect={false}
+            returnKeyType={'done'}
+            value={this.state.newTodoTitle}
+            onChangeText={this._changeTitleText}
+            onSubmitEditing={this._addTodo}
           />
-          <TouchableOpacity>
-            <Text>➕</Text>
+          <TouchableOpacity style={styles.addButton} onPress={this._addTodo}>
+            <Icon name="add" color="#bbbbbb" />
           </TouchableOpacity>
         </View>
         <ScrollView>
-          <Todo />
+          {this.state.todos.length ? (
+            this.state.todos.map(todo => (
+              <Todo
+                key={todo.createdAt}
+                todo={todo}
+                deleteTodo={this._deleteTodo}
+              />
+            ))
+          ) : (
+            <View style={styles.emptyContainer}>
+              <Text style={styles.emptyText}>Add New To-Do</Text>
+            </View>
+          )}
         </ScrollView>
       </View>
     );
@@ -42,11 +102,25 @@ const styles = StyleSheet.create({
   input: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: 20,
+    padding: 10,
     marginBottom: 10,
     backgroundColor: '#ffffff',
     borderRadius: 3,
+    elevation: 5,
   },
-  inputText: {flex: 1, fontSize: 20},
+  inputText: {flex: 10, fontSize: 20},
+  addButton: {flex: 1, alignItems: 'flex-end'},
+  emptyContainer: {
+    flex: 1,
+    borderRadius: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#723d46',
+    padding: 5,
+    elevation: 5,
+  },
+  emptyText: {
+    fontSize: 30,
+    color: '#ffffff',
+  },
 });
